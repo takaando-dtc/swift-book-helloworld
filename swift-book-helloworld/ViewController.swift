31c2d64c8f66e94c8543222cc774dcea8158abed
takaando-dtc <takaando@tohmatsu.co.jp>
2015-11-20 10:56:45 +0900

    Initial Commit

---
+++ swift-book-helloworld/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  swift-book-helloworld
+//
+//  Created by ando takahiro on 2015/11/20.
+//  Copyright © 2015年 ando takahiro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
